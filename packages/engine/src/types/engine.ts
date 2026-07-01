75cdc9c87ccda2575cad6d409342b43be011de30
LucasBBacon <lucasbrumatti@hotmail.com>
2026-07-01 13:15:43 +0100

    feat: add engine package and implement armor class calculation logic

---
+++ packages/engine/src/types/engine.ts
@@ -0,0 +1,14 @@
+export type ModTarget = "AC" | "INITIATIVE" | "SPEED" | "STR_SAVE"; // TODO: ADD REST!!
+export type ModType = "set_base" | "add" | "advantage" | "disadvantage";
+
+export interface Modifier {
+  id: string;           // unique instance ID
+  target: ModTarget;    // what is being modified
+  type: ModType;        // how it is modified
+  value: number;        // numerical payload
+
+  // metadata (FOR RULE STACKING)
+  sourceName: string;   // e.g., "Ring of Protection"
+  sourceOrigin: string; // e.g., "Item", "Class: Monk", "Spell", etc
+  isActive: boolean;    // allows toggling without deleting record
+}
