80050a36165b28b1169fe837a1844bf06bd15984
LucasBBacon <lucasbrumatti@hotmail.com>
2026-07-05 21:51:20 +0100

    Remove unused routes and services related to character and reference management
    - Deleted character.js and its associated map file.
    - Removed reference.js, reference.d.ts, and their respective map files.
    - Eliminated combatService.js and combatService.d.ts along with their map files.
    - Removed socket controller files and their definitions.

--- apps/server/src/services/combatService.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const modifyCharacterHp: (characterId: string, amount: number) => Promise<{
-    max: number;
-    current: number;
-    temporary: number;
-}>;
-//# sourceMappingURL=combatService.d.ts.map
--- apps/server/src/socket/controller.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Server } from "socket.io";
-export declare const initializeWebSockets: (io: Server) => void;
-//# sourceMappingURL=controller.d.ts.map
